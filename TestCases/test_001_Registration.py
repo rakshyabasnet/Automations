09eef647a1f4e6a0a45df2a977e7c17da983fe78
rakshyabasnet <131902000+rakshyabasnet@users.noreply.github.com>
2026-05-24 11:40:26 +0545

    Update test_001_Registration.py

--- TestCases/test_001_Registration.py
+++ TestCases/test_001_Registration.py
@@ -6,7 +6,7 @@ def test_validateRegistration():
     driver = startBrowser()
     
     # Example: Fill first name field (update XPATH)
-   # driver.find_element(By.XPATH, "//input[@name='firstname']").send_keys("elina")
-    driver.find_element('name',ElementsRead('Registration','fname')).send_keys('Deekshya')
+   # driver.find_element(By.XPATH, "//input[@name='firstname']").send_keys("Rakshya")
+    driver.find_element('name',ElementsRead('Registration','fname')).send_keys('Rakshya')
     driver.find_element('name',ElementsRead('Registration','lname')).send_keys('Basnet') 
     closeBrowser(driver)
